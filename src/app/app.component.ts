791a230291a7d9000f48cad0dd7f8c57a81af3f3
Cristian <trolese.cristian@gmail.com>
2024-06-06 20:04:17 +0100

    Prova

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'prova';
+  title:string = 'prova';
+  myname:string="cristian";
+  eta:number=28;
+  corsi:any[]=[
+    {
+      titolo:"HTML",
+      docente:"Daniele",
+      durata:36,
+      costo:600,
+      fatto:true
+    },
+    {
+      titolo:"Javascript base",
+      docente:"Domenico",
+      durata:42,
+      costo:800,
+      fatto:true
+    },
+    {
+      titolo:"Responsive",
+      docente:"Gaia",
+      durata:30,
+      costo:800,
+      fatto:true
+    },
+    {
+      titolo:"Javascript avanzato",
+      docente:"Matteo",
+      durata:42,
+      costo:600,
+      fatto:true
+    },
+    {
+      titolo:"Angular",
+      docente:"Matteo",
+      durata:36,
+      costo:1300,
+      fatto:false
+    }
+
+
+
+
+  ]
 }
